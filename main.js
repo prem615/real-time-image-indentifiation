67fe210d0cfe8b6c00d2255392825b088bdf6098
prem615 <83827170+prem615@users.noreply.github.com>
2021-11-24 19:51:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,37 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('MobileNet' , modelLoaded);
+}
+
+function draw(){
+  image(video , 0, 0 , 300 , 300);
+  classifier.classify(video ,gotResult);
+}
+
+var previous_result = "";
+
+function modelLoaded(){
+  console.log("modelloaded!!!");
 }
 
 
+function gotResult(error , results){
+  if( error){
+    console.error(error);
+  }
+  else{
+    if((results[0].confidence > 0.5  )&& (previous_result !=results[0].label)){
+      console.log(results);
+      previous_result = results[0].label;
+      var synth = window.speechSynthesis;
+      var speak_data = "object detected is" + results[0].label ;
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
 
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuray").innerHTML = results[0].confidence.toFixed(3);
+
+    }
+  }
+}
